436237c97c1dd48cbcc5b154bf9496033d953244
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2018-03-02 12:31:33 +0100

    updated kernel things

--- Exercices/5. Ex 7.py
+++ Exercices/5. Ex 7.py
@@ -45,7 +45,6 @@ def PrintMatrix(matrix):
     for y in range(0,cols):
         for x in range(0, rows):
             print matrix[y,x]
-        print
 
 img = cv2.imread('Ex_Images/ricalin.png',0)
 
@@ -55,32 +54,44 @@ def getFilteredValue(img, kernel, i, j):
     value = 0
     imgx,imgy = img.shape
     kx,ky = kernel.shape
-    for ix in range (-(kx/2),kx/2):
-        for iy in range (-(ky/2),ky/2):
+    for ix in range (-(kx/2),kx/2 +1):
+        for iy in range (-(ky/2),ky/2 +1):
             if(i+ix>=0 and i+ix < imgx and j+iy >=0 and j+ iy < imgy):
                 value = value + img[i+ix][j+iy]*kernel[ix][iy]
     return value
 
-def strechValue(value, maxV, strechValue = 255):
-    return value * strechValue / maxV
+def strechValue(value, minV, maxV, strechValue = 255):
+    return ((value - minV)/(maxV - minV))*(strechValue)
 
-def ApplyKernel(img_to_apply, kernel):    
+def copyImg(mat, img):
+    img_x, img_y = img.shape
+    for x in range(0, img_x):
+        for y in range(0, img_y):
+            mat[x][y] = img[x][y]
+
+def ApplyKernel(img_to_apply, kernel):
+    min_val = GetMinElementMatrix(img_to_apply)
     max_val = GetMaxElementMatrix(img_to_apply)
     cols, rows = img_to_apply.shape
-    new_img = img_to_apply.copy()
+    new_img = np.zeros(img_to_apply.shape)
+    copyImg(new_img, img_to_apply)
     for i in range(0,cols):
         for j in range(0,rows):
             new_img[i][j] = getFilteredValue(new_img,kernel,i,j)
+    min_val = GetMinElementMatrix(new_img)
     max_val = GetMaxElementMatrix(new_img)
+
+    print min_val
+    print max_val
+    
     for i in range(0,cols):
         for j in range(0,rows):
-            new_img[i][j] = strechValue(new_img[i][j], max_val)
+            new_img[i][j] = strechValue(new_img[i][j], min_val, max_val)
 
     return new_img
 
 kernel1 = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 kernel2 = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-print kernel1
 
 cv2.imshow('Normal', img)
 cv2.imshow('Kernel 1', ApplyKernel(img, kernel1))
